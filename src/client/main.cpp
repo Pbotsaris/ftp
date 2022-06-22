553aaffa8846b025134f8fc6e2633b9dbdbb126f
Pedro Botsaris <pedro@antfood.com>
2022-06-22 09:43:55 +0200

    socket connection barebones

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <string>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
+const int PORT = 4000;
+const char LOCALHOST[] = "127.0.0.1";
 
 int main(void)
 {
-  std::cout << "client" << std::endl;
+  struct sockaddr_in client_addr;
+
+  int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  client_addr.sin_family = AF_INET;
+  client_addr.sin_port = htons(PORT);
+
+  inet_pton(AF_INET, LOCALHOST, &client_addr.sin_addr); /* set address to localhost */
+
+  int client_fd = connect(client_socket, (struct sockaddr *) &client_addr, sizeof(client_addr));
+
+  if(client_fd < 0)
+    std::cout << "connection error" << "\n";
+
+  char msg[] = "hello from client";
+  char buffer[256] = {0};
+
+  send(client_socket, msg, strlen(msg), 0);
+  recv(client_socket, buffer, 256, 0);
+
+  std::cout << buffer << "\n";
+
+  close(client_fd);
+  close(client_socket);
 
   return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <strings.h>
 #include <unistd.h>
@@ -5,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+const int PORT = 4000;
 
 
 int main (void)
@@ -14,7 +17,7 @@ int main (void)
 
   /* domain, type, protocol */
   int socket_fd = socket(AF_INET, SOCK_STREAM, 0); /* SOCK_STREAM from TCP connection */
-  int port      = 4000;
+  int res = 0;
 
   if(socket_fd < 0)
       return 1;
@@ -22,34 +25,57 @@ int main (void)
   /* clear address struct */
   bzero(&server_addr, sizeof(server_addr));
 
+  int opt = 1;
+  res = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  if(res < 0)
+  {
+    perror("error setting options");
+    return 1;
+  }
+
+  res = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
+
+  if(res < 0)
+  {
+    perror("error setting options");
+    return 1;
+  }
+
   server_addr.sin_family      = AF_INET; /* AF_INET is the IPV4 family  */
   server_addr.sin_addr.s_addr = INADDR_ANY; /* the actual IP in the socket. Sets IP to localhost */
-  server_addr.sin_port        = htons(port); /* conver port number to network byte order */
+  server_addr.sin_port        = htons(PORT); /* conver port number to network byte order */
 
 
   if(bind(socket_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0)
-      return 1;
+    std::cout << "error binding" << "\n";
 
 
-  listen(socket_fd, 5); /* maximum size of the backlog queue is 5 */
+  res = listen(socket_fd, 5); /* maximum size of the backlog queue is 5 */
+
+  if(res < 0)
+    std::cout << "error listening" << "\n";
+
 
   socklen_t client_addr_len = sizeof(client_addr);
 
   int new_socket_fd = accept(socket_fd, (struct sockaddr*)&client_addr,  &client_addr_len);
 
-   
+  if(new_socket_fd < 0)
+    std::cout << "error accepting" << "\n";
+
   char buffer[256] = {0};
   int n = recv(new_socket_fd, buffer, sizeof(buffer)/sizeof(char), 0);
 
   if(n < 0)
-      return 1;
-   
+    return 1;
+
   std::cout << buffer << "\n";
 
   char msg[] = "Response from server";
   send(new_socket_fd,msg, strlen(msg), 0);
 
   close(new_socket_fd);
-  close(socket_fd);
+  shutdown(socket_fd, SHUT_RDWR);
   return 0;
 }
